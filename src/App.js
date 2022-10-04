b3f9d3c5f5e74c9e6765c6d7958f138b45fe86a5
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-10-04 10:16:55 -0700

    Use context to get the board in Board component
    I sort of forgot what the whole point of this exercise is.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import styles from './App.module.css';
 import Board from './components/Board/Board';
 import Controls from './components/Controls/Controls';
-import { useGame } from './context/GameContext';
 
 function App() {
-  const { board } = useGame();
-
   return <div className={styles.app}>
     <h1 className={styles.centeredText}>Tic-tac-toe</h1>
     <Controls />
-    <Board board={board} />
+    <Board />
   </div>;
 }
 
--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,7 +1,10 @@
 import styles from './Board.module.css';
 import Square from '../Square/Square';
+import { useGame } from '../../context/GameContext';
 
-export default function Board({ board }) {
+export default function Board() {
+  const { board } = useGame();
+
   return <div className={styles.board}>
     {board.map((x, i) => <Square key={i} value={x} position={i} />)}
   </div>;
